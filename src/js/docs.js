de6b52a0b6bee1ca833a31318c7097f4df73438c
DiscRiskandBisque <joshquin@gmail.com>
2019-01-09 23:02:22 -0500

    New docs data structure

--- src/js/docs.js
+++ src/js/docs.js
@@ -1,16 +1,49 @@
-$(function() {  
-  var parsedUrl = new URL(window.location.href);
+$(function() {
+  let localStorage = window.localStorage;
+
+  let currentProduct = '';
+  let currentVersion = '';
+  let currentDoc = '';
+  let parsedUrl = new URL(window.location.href);
 
   // Are we on the docs home page?
   if (parsedUrl.pathname.length > 2) {
-    var category = parsedUrl.pathname.split('/')[2];
+    // If not, expand parsed product.
+    currentProduct = parsedUrl.pathname.split('/')[2];
 
-    $('#' + category + 'Docs').collapse('show');
+    $('#' + currentProduct + 'Docs').collapse('show');
   }
 
+  // SIDEBAR
+
+  // Sidebar event handling.
   $('#docsSidebarToggle').click(function(event) {
     if ($('#docsSidebar').css('position') === 'absolute') {
       $('#docsSidebar').toggleClass('open');
     }
   });
+
+  // VERSIONING
+  
+  console.log('Current Product:');
+  console.log(currentProduct);
+  console.log('Current Version:');
+  console.log(currentVersion);
+
+  // Version Select Box
+  let $versionSelect = $('#version');
+  $versionSelect.on('change', () => handleVersionChange());
+
+  function handleVersionChange() {
+    let selectedVersion = $versionSelect.val();
+
+    // Has a new version been selected?
+    if (selectedVersion !== currentVersion) {
+      // Store the newly selected version number.
+      localStorage.setItem('truffleDocs' + currentProduct.charAt(0).toUpperCase() + 'Version', selectedVersion);
+
+      // Redirect to the appropriate page.
+      window.location.replace(window.location.protocol + '//' + window.location.hostname + '/docs/' + currentProduct + '/' + selectedVersion + '/quickstart');
+    }
+  };
 });
